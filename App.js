ae28444d4df54dd336e88fbe0027a3dde040a6d5
moumou <mouad.melhaoui@helb-prigogine.be>
2021-10-18 22:48:51 +0200

    loading screen

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//importation des différentes activités
+import LoadingScreen from "./screen/LoadingScreen"
+
+
+let content = <LoadingScreen />
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {content}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fffaf0',
   },
 });
---
+++ screen/LoadingScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, View, Image, ImageBackground} from 'react-native';
+
+
+const LoadingScreen = props => {
+    return(
+        <View style={styles.container}>
+            <View>
+                <Image source={require('../assets/logo/myEventLogo.png')} />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
+
+export default LoadingScreen;
